f0d38d2fa298c3062b1d2f0a0eee364bdee5444f
Sonny To <son.c.to@gmail.com>
2023-06-06 15:23:08 -0400

    use different logger if wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[cfg(target_arch="wasm32")]
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
 use winit::{
@@ -7,9 +7,17 @@ use winit::{
     window::WindowBuilder,
 };
 
-#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
-    env_logger::init();
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        } else {
+            env_logger::init();
+        }
+    }
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
@@ -33,4 +41,3 @@ pub fn run() {
         _ => {}
     });
 }
-
