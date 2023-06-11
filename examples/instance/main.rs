3d22b25e26b7c9244e613de9dd30147b936f2bc0
Sonny To <son.c.to@gmail.com>
2023-06-10 21:00:01 -0400

    examples

---
+++ examples/instance/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(), //wgpu::Backends::METAL,
+        dx12_shader_compiler: Default::default(),
+    });
+
+    println!("hi {:?}", instance);
+}
