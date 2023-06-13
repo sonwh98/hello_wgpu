0cb7caeca5a7f6e80abd0eb07b4c95e053f15e9c
Sonny To <son.c.to@gmail.com>
2023-06-13 15:16:15 -0400

    format code

--- examples/surface/main.rs
+++ examples/surface/main.rs
@@ -4,13 +4,12 @@ fn main() {
         .with_title("testing")
         .build(&event_loop)
         .unwrap();
-    
+
     let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
         backends: wgpu::Backends::all(), //wgpu::Backends::METAL,
         dx12_shader_compiler: Default::default(),
     });
-    
+
     let surface = unsafe { instance.create_surface(&window) }.unwrap();
     println!("surface= {:?}", surface);
 }
-
--- examples/window/main.rs
+++ examples/window/main.rs
@@ -1,9 +1,9 @@
-fn main(){
+fn main() {
     let event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::WindowBuilder::new()
         .with_title("testing")
         .build(&event_loop)
         .unwrap();
-    
+
     println!("window= {:?}", window);
 }
